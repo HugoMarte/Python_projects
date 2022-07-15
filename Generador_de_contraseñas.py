06a434e9282b999694847c3f780130dea4d1b191
HugoMarte <103032949+HugoMarte@users.noreply.github.com>
2022-07-15 10:31:02 +0200

    spetial characters added

--- Generador_de_contraseñas.py
+++ Generador_de_contraseñas.py
@@ -1,25 +1,25 @@
 import random
 
 
-def generar_contrasena ():
+def generate_password ():
     nums = ["0","1","2","3","4","5","6","7","8","9"]
-    minusculas= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    mayusculas= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    mins= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    caps= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    spetial = ["@","#","~","€","$","%"]
+    characters = nums + mins + caps + spetial
 
-    caracteres = nums + minusculas + mayusculas
-
-    contrasena = []
+    password = []
 
     for i in range (15):
-        caracter_random = random.choice (caracteres)
-        contrasena.append(caracter_random)
+        characters_random = random.choice (characters)
+        password.append(characters_random)
     
-    contrasena = "".join (contrasena)
-    return contrasena
+    password = "".join (password)
+    return password
 
 def run():
-    contrasena= generar_contrasena()
-    print ("tu nueva contrasena es"+ ""+ contrasena)
+    password= generate_password()
+    print ("Here is your new password"+ " : "+ password)
     
 
 
